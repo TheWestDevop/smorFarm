0ec6f6fc8433d30a87d228d9924d3c7cfdc25eaf
thewestdevop <maxistinlove@gmail.com>
2019-11-19 14:58:12 +0100

    Try ....

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -5,7 +5,7 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta csrf_token="{{csrf_token()}}">
+  <meta name="csrf-token" content="{{ csrf_token() }}" />
   <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
   <title>
  SmorFarm Admin
