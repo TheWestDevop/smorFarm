9030a7f8a245385ff05f1c56efce5ed9d5167841
Thewestdevop <oyeniyiadedayo@gmail.com>
2020-03-07 14:12:27 +0100

    .

--- resources/views/executor/login.blade.php
+++ resources/views/executor/login.blade.php
@@ -45,7 +45,7 @@
                             <div class="header-body text-center mb-7">
                                 <div class="row justify-content-center">
                                     <div class="col-lg-5 col-md-6">
-                                        <h1 class="text-white">Welcome Smorfarm Delivery Executor</h1>
+                                        <h1 class="text-white">Welcome Delivery Executor</h1>
                                     </div>
                                 </div>
                             </div>
