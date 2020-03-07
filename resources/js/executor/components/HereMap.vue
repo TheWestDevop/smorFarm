01b94097aeb67eab29aff728777e1015c9300b14
Thewestdevop <oyeniyiadedayo@gmail.com>
2020-03-07 18:13:10 +0100

    .

--- resources/js/executor/components/HereMap.vue
+++ resources/js/executor/components/HereMap.vue
@@ -67,7 +67,7 @@ import {
                 this.center.lat = position.coords.latitude
                 this.center.lng = position.coords.longitude
                 this.map = new H.Map(this.$refs.map,this.platform.createDefaultLayers().vector.normal.map);
-                //this.map.addLayer(defaultLayers.vector.normal.trafficincidents);
+                var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(this.map));
                 // Create an icon, an object holding the latitude and longitude, and a marker:
                 let icon = new H.map.Icon('http://maps.google.com/mapfiles/ms/icons/blue.png')
                 let marker = new H.map.Marker(this.center,{icon: icon})
