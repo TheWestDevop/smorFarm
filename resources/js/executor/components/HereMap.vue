f3e748a5aa820baea17acfefb0160ec31a519d4f
Thewestdevop <oyeniyiadedayo@gmail.com>
2020-03-07 10:09:20 +0100

    .

--- resources/js/executor/components/HereMap.vue
+++ resources/js/executor/components/HereMap.vue
@@ -28,7 +28,7 @@
     });
          },
         mounted() { 
-            this.map = new H.Map(this.$refs.map,this.platform.createDefaultLayers().normal.map);
+            this.map = new H.Map(this.$refs.map,this.platform.defaultLayers.vector.normal.map);
             this.map.setCenter({lat:this.lat, lng:this.lng});
             this.map.setZoom(17);
             console.log('position : lat' + this.lat + ' lng ' + this.lng )
